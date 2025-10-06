88fa7b9b3fd6666867be98983a1152ce65adaf06
NyAina11 <159231239+NyAina11@users.noreply.github.com>
2025-10-06 14:33:44 +0300

    Update constants.ts

--- Front-end/constants.ts
+++ Front-end/constants.ts
@@ -5,7 +5,7 @@ export const USERS: User[] = [
   { id: 'user1', name: 'Alice Dubois', role: Role.ACCUEIL },
   { id: 'user2', name: 'Bernard Martin', role: Role.GESTION },
   { id: 'user3', name: 'Catherine Petit', role: Role.CAISSE },
-  { id: 'user4', name: 'David Leroy', role: Role.CHEF_DIVISION },
+  { id: 'user4', name: 'test', role: Role.CHEF_DIVISION },
 ];
 
 export const TAX_TYPES = [
