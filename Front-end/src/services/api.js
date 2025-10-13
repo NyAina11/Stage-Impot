59ae6dbef6ac9e32237ec73de6c146e0e51bf169
NyAina11 <rakotonirinanyaina11@gmail.com>
2025-10-13 11:33:18 +0300

    BD correction

--- Front-end/src/services/api.js
+++ Front-end/src/services/api.js
@@ -20,6 +20,45 @@ apiClient.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
+// Function to convert snake_case to camelCase
+const toCamel = (s) => {
+  return s.replace(/([-_][a-z])/ig, ($1) => {
+    return $1.toUpperCase()
+      .replace('-', '')
+      .replace('_', '');
+  });
+};
+
+const isObject = (o) => {
+  return o === Object(o) && !Array.isArray(o) && typeof o !== 'function';
+};
+
+const keysToCamel = (o) => {
+  if (isObject(o)) {
+    const n = {};
+    Object.keys(o)
+      .forEach((k) => {
+        n[toCamel(k)] = keysToCamel(o[k]);
+      });
+    return n;
+  } else if (Array.isArray(o)) {
+    return o.map((i) => {
+      return keysToCamel(i);
+    });
+  }
+  return o;
+};
+
+// Response interceptor
+apiClient.interceptors.response.use((response) => {
+  if (response.data) {
+    response.data = keysToCamel(response.data);
+  }
+  return response;
+}, (error) => {
+  return Promise.reject(error);
+});
+
 // --- Auth Service ---
 export const login = (username, password) => apiClient.post('/auth/login', { username, password });
 export const register = (username, password, role) => apiClient.post('/auth/register', { username, password, role });
